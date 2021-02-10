789cae1b9dda5d3749947f378153635b61b382a3
AbelGB08 <abgonzalo@alumnos.unex.es>
2021-02-10 18:44:46 +0100

    cambios

--- key_word_v2.py
+++ key_word_v2.py
@@ -2,15 +2,22 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
+print("Grabando...")
+
 while(True):
-    with sr.Microphone() as source:
-        print("Grabando...")
+    with sr.Microphone(device_index=10) as source:
+
         r.adjust_for_ambient_noise(source)
-        data = r.record(source, duration=3)
+        data = r.listen(source)
         
         text = r.recognize_google(data,language='es')
-        print("Fin....")
+
         if "hola" in text.split(' '):
             break
+        else:
+            print("No hola..")
+
+print("Fin....")
 
 print(text)
+

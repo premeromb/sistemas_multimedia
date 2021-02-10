10b98121cb2896ed8508f45f5e6300cb563ab01b
AbelGB08 <abgonzalo@alumnos.unex.es>
2021-02-10 18:57:43 +0100

    Reconoce palabra clave y espera frase

--- key_word_v2.py
+++ key_word_v2.py
@@ -2,22 +2,36 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-print("Grabando...")
+
 
 while(True):
     with sr.Microphone(device_index=10) as source:
-
+        
+        print("Escuchando...")
         r.adjust_for_ambient_noise(source)
         data = r.listen(source)
         
-        text = r.recognize_google(data,language='es')
+        text = r.recognize_google(data, language='es-ES')
 
         if "hola" in text.split(' '):
+            print("Hola detectado ... \n")
             break
         else:
-            print("No hola..")
+            print(".....")
 
-print("Fin....")
 
-print(text)
+
+print("Di tu frase.. ")
+
+with sr.Microphone(device_index=10) as source:
+    try:
+        data = r.record(source, duration=5)
+        text = r.recognize_google(data, language='es-ES')
+        print(text)
+    except:
+        print("Nada detectado")
+
+
+
+
 
